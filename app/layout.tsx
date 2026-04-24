24381d4a0950a3db2e1384c2af895aad7a3a3b83
LorinserCC <loris3@aol.com>
2026-04-23 20:50:39 -0400

    Add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,20 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2665780342818907"
           crossOrigin="anonymous"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-S63BQH3SGC"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-S63BQH3SGC');
+            `,
+          }}
+        />
       </head>
       <body className="flex min-h-screen flex-col bg-slate-50 text-slate-900 antialiased">
         <header className="border-b border-slate-200 bg-white">
